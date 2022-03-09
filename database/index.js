ac5e836c50743f309bd99d8f2fa1b1d74f6dc1cb
mkalansari7 <mk.alansari7@gmail.com>
2022-03-09 17:09:13 +0300

    add connectionURL

--- database/index.js
+++ database/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 const connectDB = async () => {
   const DATABASE = process.env.DATABASE_NAME;
   const PASSWORD = process.env.PASSWORD;
+  // const CONNECTION_URL = `mongodb+srv://adim:${PASSWORD}@coded.j2ddu.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
   const CONNECTION_URL = `mongodb+srv://mkalansari:${PASSWORD}@coded.g97iy.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
   console.log(CONNECTION_URL);
   const conn = await mongoose.connect(CONNECTION_URL, {
