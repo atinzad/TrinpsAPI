7b714e27b901d421ae832b6c5aeb3490afe86e97
mkalansari7 <mk.alansari7@gmail.com>
2022-03-10 15:06:53 +0300

    add trip WO auth

--- apis/trips/tripRoutes.js
+++ apis/trips/tripRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const upload = require("../../middleware/multer");
-const { controllerGetTrips } = require("./tripControllers");
+const { controllerGetTrips, controllerAddTrip } = require("./tripControllers");
 
 const router = express.Router();
 
 router.get("/", controllerGetTrips);
+router.post("/", controllerAddTrip);
 
 module.exports = router;
